a7256769ad0ff49196677d7dee68128cf7f4dea9
PolledKenneth <polledkenneth031@gmail.com>
2026-03-25 08:44:21 +0800

    VERSION 11

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -9,6 +9,15 @@ const hasFirebaseConfig =
   process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID &&
   process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
 
+if (typeof window !== 'undefined' && !hasFirebaseConfig) {
+  console.error('FIREBASE CONFIG MISSING: The following env vars are required:', {
+    NEXT_PUBLIC_FIREBASE_API_KEY: !!process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: !!process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: !!process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_APP_ID: !!process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  });
+}
+
 const firebaseConfig = hasFirebaseConfig ? {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
